c62d3c197b1b20ee04ac57755ca107ae785b2ad2
Matt Brennan <mattyb1000@gmail.com>
2016-10-20 14:03:58 +0100

    init

---
+++ index.js
@@ -0,0 +1,28 @@
+const route = require('boulevard');
+const parseSlackBody = require('@quarterto/slack-body');
+
+const randomId = () => Math.floor(parseInt('zzzzzzzz', 36) * Math.random()).toString(36);
+
+module.exports = route({
+	async '/create'(req, res) {
+		const body = await parseSlackBody(req);
+		const [question, ...options] = body.text.split(',');
+		const id = randomId();
+
+		console.log(body);
+
+		return {
+			attachments: [{
+				text: question,
+				footer: 'Don\'t get excited yet, I\'m just testing',
+				callback_id: id,
+				attachment_type: 'default',
+				actions: options.map(option => ({
+					name: option.toLowerCase(),
+					text: option,
+					type: 'button',
+				}))
+			}]
+		};
+	},
+});
