bc957e4fc82c33fb0b694fbe105bf62c60c08673
Matt Brennan <mattyb1000@gmail.com>
2016-10-21 10:32:29 +0100

    don't write existing secrets

--- dotenv-secrets.js
+++ dotenv-secrets.js
@@ -1,19 +1,22 @@
 #!/usr/bin/env node
 
 const dotenv = require('dotenv');
-const {exec} = require('child_process');
+const {execSync} = require('child_process');
 const paramCase = require('param-case');
 
 const env = dotenv.config();
 
+const secrets = execSync('now secrets ls | grep sec_ | cut -d " " -f 5', {encoding: 'utf8'}).trim().split('\n');
+
 Object.keys(env).forEach(key => {
 	const secret = paramCase(key);
 	const value = env[key];
-	exec(`node_modules/.bin/now secret add "${secret}" "${value}"`, err => {
-		if(err) {
-			throw err;
-		}
 
-		console.log(`Added ${key} as @${secret}`);
-	});
+	if(secrets.includes(secret)) {
+		console.log(`Secret @${secret} already exists`);
+		return;
+	}
+
+	execSync(`node_modules/.bin/now secret add "${secret}" "${value}"`);
+	console.log(`Added ${key} as @${secret}`);
 });
